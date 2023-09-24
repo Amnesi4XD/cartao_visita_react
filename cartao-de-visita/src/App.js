bf62ffc8394bd19eb556412a8d2dc16007152374
Amnesi4XD <lucasrocha216.lr@gmail.com>
2023-09-23 21:06:03 -0300

    elementos adicionando cartao de visita

---
+++ cartao-de-visita/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Cartao from './Cartao';
+
+function App() {
+  return (
+    <div className="App">
+      <Cartao />
+    </div>
+  );
+}
+
+export default App;
---
+++ cartao-de-visita/src/Cartao.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Cartao.css';
+
+function CartaoDeVisita() {
+  return (
+    <div className="cartao">
+      <img src="darth_vader.jpeg" alt="Foto do cartão" />
+      <h1>Lucas Rocha</h1>
+      <p>Estudante de Ciência da Computação no IDP</p>
+      Github: <a href="https://github.com/Amnesi4XD">https://github.com/Amnesi4XD</a>    </div>
+  );
+}
+
+export default CartaoDeVisita;
